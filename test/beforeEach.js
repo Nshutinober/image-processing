bfd93956cd0c7fcf1c0f61b132a9e3f7c1307b34
Eng.Nobert <mukankusiannoncita@gmail.com>
2024-01-20 03:44:22 +0300

    Initial commit

---
+++ test/beforeEach.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const sharp = require("../");
+
+const usingCache = !process.env.G_DEBUG;
+const usingSimd = !process.env.VIPS_NOVECTOR;
+const concurrency = Number(process.env.VIPS_CONCURRENCY) || 0;
+
+exports.mochaHooks = {
+  beforeEach() {
+    sharp.cache(usingCache);
+    sharp.simd(usingSimd);
+    sharp.concurrency(concurrency);
+  },
+
+  afterEach() {
+    if (global.gc) {
+      global.gc();
+    }
+  },
+};
---
+++ test/unit/heif.js
@@ -0,0 +1,81 @@
+// Copyright 2013 Lovell Fuller and others.
+// SPDX-License-Identifier: Apache-2.0
+
+'use strict';
+
+const assert = require('assert');
+
+const sharp = require('../../');
+
+describe('HEIF', () => {
+  it('called without options throws an error', () => {
+    assert.throws(() => {
+      sharp().heif();
+    });
+  });
+  it('valid quality does not throw an error', () => {
+    assert.doesNotThrow(() => {
+      sharp().heif({ compression: 'av1', quality: 80 });
+    });
+  });
+  it('invalid quality should throw an error', () => {
+    assert.throws(() => {
+      sharp().heif({ compression: 'av1', quality: 101 });
+    });
+  });
+  it('non-numeric quality should throw an error', () => {
+    assert.throws(() => {
+      sharp().heif({ compression: 'av1', quality: 'fail' });
+    });
+  });
+  it('valid lossless does not throw an error', () => {
+    assert.doesNotThrow(() => {
+      sharp().heif({ compression: 'av1', lossless: true });
+    });
+  });
+  it('non-boolean lossless should throw an error', () => {
+    assert.throws(() => {
+      sharp().heif({ compression: 'av1', lossless: 'fail' });
+    });
+  });
+  it('valid compression does not throw an error', () => {
+    assert.doesNotThrow(() => {
+      sharp().heif({ compression: 'hevc' });
+    });
+  });
+  it('unknown compression should throw an error', () => {
+    assert.throws(() => {
+      sharp().heif({ compression: 'fail' });
+    });
+  });
+  it('invalid compression should throw an error', () => {
+    assert.throws(() => {
+      sharp().heif({ compression: 1 });
+    });
+  });
+  it('valid effort does not throw an error', () => {
+    assert.doesNotThrow(() => {
+      sharp().heif({ compression: 'av1', effort: 6 });
+    });
+  });
+  it('out of range effort should throw an error', () => {
+    assert.throws(() => {
+      sharp().heif({ compression: 'av1', effort: 10 });
+    });
+  });
+  it('invalid effort should throw an error', () => {
+    assert.throws(() => {
+      sharp().heif({ compression: 'av1', effort: 'fail' });
+    });
+  });
+  it('invalid chromaSubsampling should throw an error', () => {
+    assert.throws(() => {
+      sharp().heif({ compression: 'av1', chromaSubsampling: 'fail' });
+    });
+  });
+  it('valid chromaSubsampling does not throw an error', () => {
+    assert.doesNotThrow(() => {
+      sharp().heif({ compression: 'av1', chromaSubsampling: '4:4:4' });
+    });
+  });
+});
